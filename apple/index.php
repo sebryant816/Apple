1cb1bf385ce4a2eacb44c23b840d06102aae7b7d
Stephen Bryant <sebryant816@yahoo.com>
2019-05-29 11:35:19 -0400

    Update index.php

--- apple/index.php
+++ apple/index.php
@@ -11,155 +11,141 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
-<title>APPLE 13inch</title>
+    <title>APPLE 13inch</title>
 
       <!-- Latest compiled and minified CSS -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
 
-<!-- jQuery library -->
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <!-- jQuery library -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 
-<!-- Popper JS -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+        <!-- Popper JS -->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
 
-<!-- Latest compiled JavaScript -->
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+        <!-- Latest compiled JavaScript -->
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
 
+        <link href="https://fonts.googleapis.com/css?family=Pacifico" rel="stylesheet">
 
-      <link href="https://fonts.googleapis.com/css?family=Pacifico" rel="stylesheet">
-      
-      <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
-      <link rel="shortcut icon" type="image/x-icon" href="favicon.ico">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+        <link rel="shortcut icon" type="image/x-icon" href="favicon.ico">
+ 
+<style>
 
-
-
-  <style>
-
-
-  </style>
+</style>
 
 </head>
 
 <body>
  
-<?php
-$servername = "localhost";
-$username = "jaxcode83";
-$password = "Ducks0up";
-$dbname = "jaxcode83";
+                <?php
 
-// Create connection
-$conn = mysqli_connect($servername, $username, $password, $dbname);
-// Check connection
-if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
-}
-?>
+                // Create connection
+                $conn = mysqli_connect($servername, $username, $password, $dbname);
+                // Check connection
+                if (!$conn) {
+                    die("Connection failed: " . mysqli_connect_error());
+                }
+                ?>
 
-<!--Nav-->
-<nav class="navbar navbar-expand-sm navbar-dark bg-dark justify-content-center py-0">
-<div class="container">
-<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar10">
-<span class="navbar-toggler-icon"></span>
-</button>
-<div class="navbar-collapse collapse" id="navbar10">
-<ul class="navbar-nav nav-fill w-100">
-<li class="nav-item">
-<a class="nav-link" href="#" style="color:white;font-family: Arial Narrow;"><i class="fab fa-apple fa-1x" style="font-size:21px;"></i></a>
-</li>
-<li class="nav-item">
-<a class="nav-link" href="#" style="color:white;font-family: Arial Narrow;">Mac</a>
-</li>
-<li class="nav-item">
-<a class="nav-link" href="#" style="color:white;font-family: Arial Narrow;">iPad</a>
-</li>
-<li class="nav-item">
-<a class="nav-link" href="#" style="color:white;font-family: Arial Narrow;">iPhone</a>
-</li>
-<li class="nav-item">
-<a class="nav-link" href="#" style="color:white;font-family: Arial Narrow;">Watch</a>
-</li>
-<li class="nav-item">
-<a class="nav-link" href="#" style="color:white;font-family: Arial Narrow;">TV</a>
-</li>
-<li class="nav-item">
-<a class="nav-link" href="#" style="color:white;font-family: Arial Narrow;">Music</a>
-</li>
-<li class="nav-item">
-<a class="nav-link" href="#" style="color:white;font-family: Arial Narrow;">Support</a>
-</li>
-<li class="nav-item">
-<a class="nav-link" href="#" style="color:white;font-family: Arial Narrow;"><i class="fas fa-search fa-1x"></i></a>
-</li>
-<li class="nav-item">
-<a class="nav-link" href="#" style="color:white;font-family: Arial Narrow;"><i class="fas fa-shopping-bag"></i></a>
-</li>
-</ul>
-</div>
-</div>
-</nav>
+                <!--Nav-->
+    <nav class="navbar navbar-expand-sm navbar-dark bg-dark justify-content-center py-0">
+        <div class="container">
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar10">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+           <div class="navbar-collapse collapse" id="navbar10">
+                        <ul class="navbar-nav nav-fill w-100">
+                        <li class="nav-item">
+                        <a class="nav-link" href="#" style="color:white;font-family: Arial Narrow;"><i class="fab fa-apple fa-1x" style="font-size:21px;"></i></a>
+                        </li>
+                        <li class="nav-item">
+                        <a class="nav-link" href="#" style="color:white;font-family: Arial Narrow;">Mac</a>
+                        </li>
+                        <li class="nav-item">
+                        <a class="nav-link" href="#" style="color:white;font-family: Arial Narrow;">iPad</a>
+                        </li>
+                        <li class="nav-item">
+                        <a class="nav-link" href="#" style="color:white;font-family: Arial Narrow;">iPhone</a>
+                        </li>
+                        <li class="nav-item">
+                        <a class="nav-link" href="#" style="color:white;font-family: Arial Narrow;">Watch</a>
+                        </li>
+                        <li class="nav-item">
+                        <a class="nav-link" href="#" style="color:white;font-family: Arial Narrow;">TV</a>
+                        </li>
+                        <li class="nav-item">
+                        <a class="nav-link" href="#" style="color:white;font-family: Arial Narrow;">Music</a>
+                        </li>
+                        <li class="nav-item">
+                        <a class="nav-link" href="#" style="color:white;font-family: Arial Narrow;">Support</a>
+                        </li>
+                        <li class="nav-item">
+                        <a class="nav-link" href="#" style="color:white;font-family: Arial Narrow;"><i class="fas fa-search fa-1x"></i></a>
+                        </li>
+                        <li class="nav-item">
+                        <a class="nav-link" href="#" style="color:white;font-family: Arial Narrow;"><i class="fas fa-shopping-bag"></i></a>
+                        </li>
+                        </ul>
+          </div>
+        </div>
+    </nav>
 
 <!--container for second menu-->
 
-<div class="container">
-  <div class="row">
-        <div class="col-md-6 text-left" style="font-size:25px;">
-      <strong>MacBook Pro</strong>
-  </div> <!--/col-->
-      <div class="col-md-6 text-right">
-    <a href="http://hello.sebryant816.webfactional.com/apple/specs.php"> Overview    macOS   Tech Specs</a>
-    </div> <!--/col-->
+        <div class="container">
+          <div class="row">
+             <div class="col-md-6 text-left" style="font-size:25px;">
+              <strong>MacBook Pro</strong>
+            </div> <!--/col-->
+            <div class="col-md-6 text-right">
+                <a href="http://hello.sebryant816.webfactional.com/apple/specs.php"> Overview    macOS   Tech Specs</a>
+            </div> <!--/col-->
+          </div> <!--/row-->
+        </div> <!--/container-->
 
 
-    
-  </div> <!--/row-->
-</div> <!--/container-->
+        <div class="container" style="border-top:1px black solid;">
+          <div class="row">
+             <div class="col-md-12 text-center"><br>
+                <span class="lead" style="font-size:25px;"><strong>Add a trade-in</strong></span>
+                <br>
+                    <p style="font-size:15px;">Get a refund of up to $1100 when you trade in an eligible computer, or recycle it for free.<sup>*<sup></p>
+                    <p class="lead" style="border-bottom:1px solid gray;"> <a href="#">Get started</a> </p>
+             </div> <!--/col-->
+          </div> <!--/row-->
+        </div> <!--/container-->
 
+        <div class="container" > 
+          <div class="row">
+            <div class="col-md-12 text-center"><br>
+            <span class="lead" style="font-size:40px;"><strong>Choose your new MacBook Pro<br>and select a finish.</strong></span>
+            </div> <!--/col-->
+          </div> <!--/row-->
+        </div> <!--/container-->
 
-<div class="container" style="border-top:1px black solid;">
-  <div class="row">
-        <div class="col-md-12 text-center"><br>
-        <span class="lead" style="font-size:25px;"><strong>Add a trade-in</strong></span>
-        <br>
-            <p style="font-size:15px;">Get a refund of up to $1100 when you trade in an eligible computer, or recycle it for free.<sup>*<sup></p>
-            <p class="lead" style="border-bottom:1px solid gray;"> <a href="#">Get started</a> </p>
-        </div> <!--/col-->
+        <!--button here-->
+                <br><br>
 
-  </div> <!--/row-->
-</div> <!--/container-->
+        <div class="container  text-center">
+          <div class="row">
+            <div class="col-md-12">
+            <a href="index.php" class="btn btn-info" role="button"style="border: 1px solid black;font-size:18px;padding:40px; background-color:#4d94ff;color:white; width:175px; height:70px;"><strong>13-inch</strong></a>
+            <a href="15inch.php" class="btn btn-info" role="button"style="border: 1px solid black;font-size:18px;padding:40px; background-color:#4d94ff;color:white; width:175px; height:70px;"><strong>15-inch</strong></a>
+            </div>
+          </div>
+        </div>
 
+        <br><br><br><br>
 
-<div class="container" > 
-  <div class="row">
-    <div class="col-md-12 text-center"><br>
-    <span class="lead" style="font-size:40px;"><strong>Choose your new MacBook Pro<br>and select a finish.</strong></span>
-    </div> <!--/col-->
-  </div> <!--/row-->
-</div> <!--/container-->
-
-<!--button here-->
-
-<br><br>
-
-<div class="container  text-center">
-  <div class="row">
-    <div class="col-md-12">
-    <a href="index.php" class="btn btn-info" role="button"style="border: 1px solid black;font-size:18px;padding:40px; background-color:#4d94ff;color:white; width:175px; height:70px;"><strong>13-inch</strong></a>
-    <a href="15inch.php" class="btn btn-info" role="button"style="border: 1px solid black;font-size:18px;padding:40px; background-color:#4d94ff;color:white; width:175px; height:70px;"><strong>15-inch</strong></a>
-    </div>
-  </div>
-</div>
-
-<br><br><br><br>
-
-<div class="container  text-center"style="padding-bottom:45px;">
-  <div class="row">
-   <div class="col-md-12">
-      <span class="lead" style="font-size:32px;"><strong>13-inch MacBook Pro</strong></span><br>
-      <span class="lead" style="font-size:15px;">Were here to help <a href="#">Contact us.</a></span>
-    </div>
-  </div>
-</div>
+        <div class="container  text-center"style="padding-bottom:45px;">
+          <div class="row">
+           <div class="col-md-12">
+              <span class="lead" style="font-size:32px;"><strong>13-inch MacBook Pro</strong></span><br>
+              <span class="lead" style="font-size:15px;">Were here to help <a href="#">Contact us.</a></span>
+            </div>
+          </div>
+        </div>
 
 
 <!--column 1-->
@@ -528,98 +514,92 @@ if (!$conn) {
 
    <!--Footer third section col 4-->
 
-     <div class="col-md-2" style="padding-left:40px;">
-       <table>
-    
-          <tr>
+      <div class="col-md-2" style="padding-left:40px;">
+          <table>
+              <tr>
 
-            <?php
+                    <?php
 
-                $sql = "SELECT * FROM apple WHERE id=9";
-                $result = mysqli_query($conn, $sql);
+                        $sql = "SELECT * FROM apple WHERE id=9";
+                        $result = mysqli_query($conn, $sql);
 
-                if (mysqli_num_rows($result) > 0) {
-                
-                // output data of each row
-                
-                while($row = mysqli_fetch_assoc($result)) {
-                    ?>
-                    <td><?=$row["description"]?></td>
-                    <?
-                  }
-                } else {
-                  echo "0 results";
-                }
-              
-             ?>
-           </tr>
-       </table>
+                        if (mysqli_num_rows($result) > 0) {
+
+                        // output data of each row
+
+                        while($row = mysqli_fetch_assoc($result)) {
+                            ?>
+                            <td><?=$row["description"]?></td>
+                            <?
+                          }
+                        } else {
+                          echo "0 results";
+                        }
+
+                     ?>
+             </tr>
+         </table>
           
       </div>
       <!--Footer third section col 5-->
 
      <div class="col-md-2" style="padding-left:50px;">
-       <table>
-    
-          <tr>
+           <table>
+              <tr>
 
-            <?php
+                <?php
 
-                $sql = "SELECT * FROM apple WHERE id=10";
-                $result = mysqli_query($conn, $sql);
+                    $sql = "SELECT * FROM apple WHERE id=10";
+                    $result = mysqli_query($conn, $sql);
 
-                if (mysqli_num_rows($result) > 0) {
-                
-                // output data of each row
-                
-                while($row = mysqli_fetch_assoc($result)) {
-                    ?>
-                    <td><?=$row["description"]?></td>
-                    <?
-                  }
-                } else {
-                  echo "0 results";
-                }
-              
-                mysqli_close($conn);
-             ?>
-           </tr>
-       </table>
-          
+                    if (mysqli_num_rows($result) > 0) {
+
+                    // output data of each row
+
+                    while($row = mysqli_fetch_assoc($result)) {
+                        ?>
+                        <td><?=$row["description"]?></td>
+                        <?
+                      }
+                    } else {
+                      echo "0 results";
+                    }
+
+                    mysqli_close($conn);
+                 ?>
+               </tr>
+           </table>
+
       </div>
       <!--Footer third section col 6-->
 
-      <div class="col-md-2"></div>
+         <div class="col-md-2"></div>
 
-
-    </div>
+      </div>
     <br><br>
 
 
     <p style="margin-left:18px; color:gray">More ways to shop: Visit an <a href="#">Apple Store<a>, call 1‑800‑MY‑APPLE, or <a href="#">find a reseller</a>.</p>
   
           <!--final footer section-->
-    <div class="container">
-      <div class="row">
-        <div class="col-md-10">
+   <div class="container">
+     <div class="row">
+       <div class="col-md-10">
 
-        <div style="background-color:#EAEDED; color:gray; font-size:12px;"> <p>Copyright © 2019 Apple Inc. All rights reserved.<span style="color: lightgray"> ></span> Privacy Policy |<span style="color: lightgray"> ></span> Terms of Use |<span style="color: lightgray"> ></span> <span>Sales and Refunds |<span style="color: lightgray"> ></span> <span>Legal |<span style="color: lightgray"> ></span> <span>Site Map <span style="color: lightgray"> </span></p></div>
+       <div style="background-color:#EAEDED; color:gray; font-size:12px;"> <p>Copyright © 2019 Apple Inc. All rights reserved.<span style="color: lightgray"> ></span> Privacy Policy |<span style="color: lightgray"> ></span> Terms of Use |<span style="color: lightgray"> ></span> <span>Sales and Refunds |<span style="color: lightgray"> ></span> <span>Legal |<span style="color: lightgray"> ></span> <span>Site Map <span style="color: lightgray"> </span></p></div>
             
-     </div>
+       </div>
   
 
 <!--final footre col  2 --->
 
-<div class="col-md-2">
+       <div class="col-md-2">
 
           <img src="images/us.png" style="width: 100%;" alt="" > 
         </div>
       </div>
-</div>
-
-
-            
-
+    </div>
+           
 <br>
 </body>
 
